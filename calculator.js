41a5164c084f4970c8db15a83f7dc08a0dd5e893
gkstjswo12 <gkstjswo122@gmail.com>
2025-06-04 09:36:04 +0900

    modified

--- calculator.js
+++ calculator.js
@@ -6,10 +6,10 @@ const minus = (a,b) => {
     return a-b
 }
 
-const add = (a,b) => {
-    return a+b
+const devine = (a,b) => {
+    return a/b
 }
 
-const add = (a,b) => {
+const cal = (a,b) => {
     return a+b
 }
